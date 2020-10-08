054f3fa32b99393af7e2480e8fa8abe4521211f6
wilschok <william.schokkele@howest.be>
2020-10-08 09:17:01 +0200

    Add project files.

---
+++ RazorPagesDemo/Data/RazorPagesDemoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesDemo.Models;
+
+namespace RazorPagesDemo.Data
+{
+    public class RazorPagesDemoContext : DbContext
+    {
+        public RazorPagesDemoContext (DbContextOptions<RazorPagesDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesDemo.Models.Movie> Movie { get; set; }
+    }
+}
