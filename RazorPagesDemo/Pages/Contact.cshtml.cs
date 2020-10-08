0ad1f4b47fe949f1fe4d9050f7ae0148cbe4d22c
wilschok <william.schokkele@howest.be>
2020-10-08 10:02:06 +0200

    Update Contact Page

--- RazorPagesDemo/Pages/Contact.cshtml.cs
+++ RazorPagesDemo/Pages/Contact.cshtml.cs
@@ -7,10 +7,22 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPagesDemo.Pages
 {
+    [BindProperties]
     public class ContactModel : PageModel
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public string Feedback { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public void OnPost() 
+        { 
+
+            Feedback = $"Bedankt {Name} voor je bericht. We sturen zo snel mogelijk een bericht naar {Email}"; 
         }
     }
 }
